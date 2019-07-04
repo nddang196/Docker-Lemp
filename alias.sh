f2ebbba605c9b0f484ed095df4b9e3516e5901fc
VooThanhPhong <nddang196@gmail.com>
2019-07-04 18:35:40 +0700

    Add mysqli ext to php image

--- alias.sh
+++ alias.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 printf "
-alias redis="docker-compose exec redis"
-alias elasticsearch="docker-compose exec elasticsearch"
-alias n98="docker-compose exec -u www php mgt"
-alias magento="docker-compose exec -u www php bin/magento"
-alias artisan="docker-compose exec -u www php php artisan"
-alias php="docker-compose exec -u www php php"
-alias composer="doco exec -u www php composer"
+alias redis=\"docker-compose exec redis\"
+alias elasticsearch=\"docker-compose exec elasticsearch\"
+alias n98=\"docker-compose exec -u www php mgt\"
+alias magento=\"docker-compose exec -u www php bin/magento\"
+alias artisan=\"docker-compose exec -u www php php artisan\"
+alias php=\"docker-compose exec -u www php php\"
+alias composer=\"doco exec -u www php composer\"
 " >> ~/.zshrc
